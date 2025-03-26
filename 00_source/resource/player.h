a463a0009eba80d3c7279bfd83e7f8a398508670
YuichFujita <221065@st.yoshida-g.ac.jp>
2025-03-26 10:37:48 +0900

    player:左右移動のみに、ジャンプ追加、ポリゴンに fade:ゲームから開始

--- 00_source/resource/player.h
+++ 00_source/resource/player.h
@@ -13,44 +13,15 @@
 //************************************************************
 //	インクルードファイル
 //************************************************************
-#include "objectChara.h"
+#include "object3D.h"
 
 //************************************************************
 //	クラス定義
 //************************************************************
 // プレイヤークラス
-class CPlayer : public CObjectChara
+class CPlayer : public CObject3D
 {
 public:
-	// モデル列挙
-	enum EModel
-	{
-		MODEL_WAIST = 0,	// 腰
-		MODEL_BODY,			// 体
-		MODEL_HEAD,			// 頭
-		MODEL_ARMUL,		// 左上腕
-		MODEL_ARMUR,		// 右上腕
-		MODEL_ARMDL,		// 左下腕
-		MODEL_ARMDR,		// 右下腕
-		MODEL_HANDL,		// 左手
-		MODEL_HANDR,		// 右手
-		MODEL_LEGUL,		// 左太もも
-		MODEL_LEGUR,		// 右太もも
-		MODEL_LEGDL,		// 左脛
-		MODEL_LEGDR,		// 右脛
-		MODEL_FOOTL,		// 左足
-		MODEL_FOOTR,		// 右足
-		MODEL_MAX			// この列挙型の総数
-	};
-
-	// モーション列挙
-	enum EMotion
-	{
-		MOTION_IDOL = 0,	// 待機モーション
-		MOTION_WALK,		// 歩行モーション
-		MOTION_MAX			// この列挙型の総数
-	};
-
 	// 状態列挙
 	enum EState
 	{
@@ -97,22 +68,22 @@ public:
 
 private:
 	// エイリアス定義
-	typedef EMotion(CPlayer::*AFuncState)(const float);	// 状態更新関数ポインタ
+	typedef void(CPlayer::*AFuncState)(const float);	// 状態更新関数ポインタ
 
 	// 関数配列
 	static AFuncState m_aFuncState[];	// 状態更新関数リスト
 
 	// メンバ関数
-	EMotion UpdateNone(const float fDeltaTime);		// 何もしない状態時の更新
-	EMotion UpdateNormal(const float fDeltaTime);	// 通常状態時の更新
-	EMotion UpdateDeath(const float fDeltaTime);	// 死亡状態時の更新
-	EMotion UpdateMove(const float fDeltaTime);		// 移動量/目標向きの更新
-	void UpdateOldPosition();						// 過去位置の更新
-	void UpdateGravity(const float fDeltaTime);		// 重力の更新
+	void UpdateNone(const float fDeltaTime);	// 何もしない状態時の更新
+	void UpdateNormal(const float fDeltaTime);	// 通常状態時の更新
+	void UpdateDeath(const float fDeltaTime);	// 死亡状態時の更新
+	void UpdateMove(const float fDeltaTime);	// 移動量/目標向きの更新
+	void UpdateOldPosition();					// 過去位置の更新
+	void UpdateGravity(const float fDeltaTime);	// 重力の更新
+	void UpdateJump(const float fDeltaTime);	// ジャンプの更新
 	bool UpdateLanding(VECTOR3* pPos, const float fDeltaTime);	// 着地状況の更新
 	void UpdatePosition(VECTOR3* pPos, const float fDeltaTime);	// 位置の更新
 	void UpdateRotation(VECTOR3* pRot, const float fDeltaTime);	// 向きの更新
-	void UpdateMotion(int nMotion, const float fDeltaTime);		// モーション/キャラクターの更新
 
 	// 静的メンバ変数
 	static CListManager<CPlayer>* m_pList;	// オブジェクトリスト
